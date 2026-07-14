2dba90bc21bdea38b44a814102d743a87252aba5
shiwangdewangan194 <shiwangdewangan194@gmail.com>
2026-07-15 04:23:27 +0530

    Completed Eyes section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
+import Eyes from './components/Eyes'
+import Features from './components/Features'
 const App = () => {
   return (
     <div className = " w-full min-h-screen  text-white bg-zinc-900">
@@ -10,6 +12,8 @@ const App = () => {
       <LandingPage/>
       <Marquee/>
       <About/>
+      <Eyes/>
+      <Features/>
     </div>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,21 @@ import React from "react";
 const About = () => {
   return (
     <div className="w-full  p-20 bg-[#CDEA68] rounded-3xl text-black">
-      <h1 className="font-['Neue Montreal'] text-[4vw] leading-[5vw] tracking-tight ">
+      <h1 className=" mb-15 font-['Neue Montreal'] text-[4vw] leading-[5vw] tracking-tight ">
         We craft category-defining presentations, brand identities, and digital
         experiences that drive funding, sales, and market leadership.
       </h1>
+      <div className="w-ful flex gap-5 border-t-[2px]  border-[#60770d] ">
+        <div className="w-1/2 ">
+          <h1 className="text-6xl mt-20">Our Approach</h1>
+          <button className="flex gap-10 uppercase items-center mt-10 bg-zinc-900 text-white px-10 py-6 rounded-full">
+            Read More
+            <div className="w-4 h-4 bg-zinc-100 rounded-full "></div>
+          </button>
+        </div>
+        <div className="w-1/2 rounded-3xl h-[70vh] mt-10 bg-red-500">
+         </div>
+      </div>
     </div>
   );
 };
